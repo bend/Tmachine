a2f6bd095c3b1e5b8c96f6146ffc0aba3120995a
Bend <ben.daccache@gmail.com>
2011-05-24 21:57:03 +0200

    first import

---
+++ Node.rb
@@ -0,0 +1,32 @@
+#!/usr/local/bin/ruby
+
+class Node
+
+	attr_accessor :next
+	attr_accessor :prev
+	attr_accessor :elem
+
+public
+	
+	def initialize
+		@next = nil
+		@prev = nil
+		@elem = nil
+	end
+
+	def setNext(node)
+		@next = node
+		node.prev=self
+	end
+
+	def setPrev(node)
+		@prev = node
+		node.next=self
+	end
+
+	def setElem(elem)
+		@elem = elem
+	end
+
+end
+
---
+++ Tape.rb
@@ -0,0 +1,83 @@
+#!/usr/local/bin/ruby
+require 'Node.rb'
+
+class Tape 
+
+	attr_accessor :current
+	attr_accessor :head
+
+	def initialize
+		@head= Node.new
+		head.setElem('')
+		@current = head
+	end
+
+	def fillTape(init)
+		init.each_char do |e|
+			node = Node.new
+			node.setElem(e)
+			current.setNext(node)
+			@current = node
+		end
+		@current = head	
+	end
+
+	def putSymbol(s)
+		current.setElem(s)
+	end
+
+	def leftMove
+		if current.prev != nil
+			@current = current.prev
+		else
+			node = Node.new
+			node.setNext(current)
+			@current = node
+		end
+	end
+	
+	def rightMove
+		if current.next != nil
+			@current = current.next
+		else
+			node = Node.new
+			node.setPrev(current)
+			@current = node
+		end
+	end
+
+	def getUnderCur
+		return current.elem
+	end
+
+	def toString
+		cur = head
+		str_buf=""
+		while cur != nil
+			str_buf+=cur.elem
+			cur = cur.next
+		end
+		return str_buf
+	end
+
+end
+
+t = Tape.new
+t.fillTape("abcdefghijkl")
+puts t.toString
+puts t.getUnderCur
+t.rightMove
+puts t.getUnderCur
+
+t.rightMove
+puts t.getUnderCur
+t.rightMove
+puts t.getUnderCur
+t.rightMove
+puts t.getUnderCur
+t.rightMove
+puts t.getUnderCur
+t.leftMove
+puts t.getUnderCur
+t.leftMove
+puts t.getUnderCur
